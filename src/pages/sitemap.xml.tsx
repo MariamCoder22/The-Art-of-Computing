eba0bdfc4270061c49e23ab369d585232e6db3a8
Mariam Khayr <mariamkhayr8@gmail.com>
2025-04-19 21:19:17 -0500

    edit

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -7,7 +7,6 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const posts = await getPosts()
   const dynamicPaths = posts.map((post) => `${CONFIG.link}/${post.slug}`)
 
-  // Create an array of fields, each with a loc and lastmod
   const fields: ISitemapField[] = dynamicPaths.map((path) => ({
     loc: path,
     lastmod: new Date().toISOString(),
@@ -15,7 +14,6 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     changefreq: "daily",
   }))
 
-  // Include the site root separately
   fields.unshift({
     loc: CONFIG.link,
     lastmod: new Date().toISOString(),
@@ -26,5 +24,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   return getServerSideSitemap(ctx, fields)
 }
 
-// Default export to prevent next.js errors
-export default () => {}
+// ✅ Named export, then exported as default
+const Sitemap = () => {}
+
+export default Sitemap
