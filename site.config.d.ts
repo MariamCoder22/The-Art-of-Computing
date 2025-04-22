4c405acfbc40f5d6112727fb4247ac945761d9e7
Mariam Khayr <mariamkhayr8@gmail.com>
2025-04-21 20:41:40 -0500

    add new file for config insta issue

---
+++ site.config.d.ts
@@ -0,0 +1,23 @@
+// site.config.d.ts
+
+export interface Profile {
+    name: string
+    image: string
+    role: string
+    bio: string
+    email: string
+    linkedin: string
+    github: string
+    instagram?: string
+  }
+  
+  export interface SiteConfig {
+    profile: Profile
+    // You can extend this with other config sections if needed:
+    // blog, projects, notionConfig, etc.
+  }
+  
+  declare module "site.config" {
+    export const CONFIG: SiteConfig
+  }
+  
